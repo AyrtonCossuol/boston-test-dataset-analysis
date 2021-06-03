f743adc4992c3976c022ce98db5f45b6c4a58a8f
Ayrton Saué Cossuol <ayrton.cossuol@live.com>
2021-06-02 21:34:41 -0300

    :hammer: modifying table views and adding the map

--- house-sales-king-county-USA/streamlit_app.py
+++ house-sales-king-county-USA/streamlit_app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
 
 st.set_page_config(layout = 'wide')
 
@@ -34,6 +36,9 @@ elif f_zipcode == [] and f_attributes != []:
 else:
     data = data.copy()
 
+st.dataframe(data)
+
+c1, c2 = st.beta_columns((1, 1))
 # Average metrics
 df1 = data[['id', 'zipcode']].groupby('zipcode').count().reset_index()
 df2 = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
@@ -46,7 +51,9 @@ m2 = pd.merge(m1, df3, on = 'zipcode', how = 'inner')
 df = pd.merge(m2, df4, on = 'zipcode', how = 'inner')
 
 df.columns = ['zipcode', 'total_houses', 'price', 'sqrt_living', 'price_m2']
-st.dataframe(df, height = 500)
+
+c1.header('Average Values')
+c1.dataframe(df, height = 500)
 
 # Statistic Descriptive
 num_attributes = data.select_dtypes(include = ['int64', 'float64'])
@@ -60,5 +67,23 @@ min_ = pd.DataFrame(num_attributes.apply(np.min))
 df1 = pd.concat([max_, min_, media, mediana, std], axis = 1).reset_index()
 df1.columns = ['attributes', 'max', 'min', 'mean', 'median', 'std']
 
-st.dataframe(df1, height = 600)
-# st.write(df.head())
+c2.header('Descriptive Analysis')
+c2.dataframe(df1, height = 500)
+
+# =============================
+# Portfolio density
+# =============================
+st.title('Region Overview')
+
+c1, c2 = st.beta_columns((1, 1))
+c1.header('Portifolio Density')
+
+df = data.sample(10)
+
+# Base Map - Folium
+density_map = folium.Map(location = [
+                            data['lat'].mean(), 
+                            data['long'].mean()],
+                            default_zoom_star = 15)
+with c1:
+    folium_static(density_map)
